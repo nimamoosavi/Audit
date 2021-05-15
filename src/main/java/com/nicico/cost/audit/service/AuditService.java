dc784dc673f9a5086f8a8eed45d387fe4485c48b
nima <nmoosavi@live.com>
2021-05-15 14:36:25 +0430

    add KAFKA to audit

--- src/main/java/com/nicico/cost/audit/service/AuditService.java
+++ src/main/java/com/nicico/cost/audit/service/AuditService.java
@@ -2,6 +2,11 @@ package com.nicico.cost.audit.service;
 
 import com.nicico.cost.framework.packages.audit.Log;
 
+/**
+ * @version 1.0.1
+ * @apiNote default log store in file but you can change it by properties audit.log.location
+ * and default log Topic is in audit but you can change it by set properties audit.log.topic
+ */
 public interface AuditService extends Log {
 
 }
